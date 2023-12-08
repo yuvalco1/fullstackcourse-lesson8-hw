ce03de18e9950b79a9709a79f4815d3721a584d0
Yuval Cohen <yuvalco1@gmail.com>
2023-12-08 15:50:10 +0200

    added home work - Printer class

--- src/Main.java
+++ src/Main.java
@@ -48,6 +48,16 @@ public class Main {
         tony.showInfo();
         yuval.showInfo();
 
+        Printer printer = new Printer(50, true);
+        System.out.println(printer.addToner(50));
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = "+ printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer = "+ printer.getPagesPrinted());
+
+
+
 
     }
 }
